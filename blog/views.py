8987f1149f8185f5579e7a81faf54eb8d64995aa
Prithvi2310 <shahprithvi1505@gmail.com>
2024-07-02 00:42:48 +0530

    created django models and ran migrations; made changes to the templates and blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Prithvi Shah',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date': '27/06/24' 
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date': '26/06/24' 
-    }
-    
-]
+from .models import post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
